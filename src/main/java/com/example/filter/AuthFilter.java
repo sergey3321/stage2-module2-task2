28334ff67d937626859666d851dd5a9be2c9f0af
sergey3321 <mail.sergey21@gmail.com>
2024-09-24 14:47:31 +0300

    stage2-module2-task2

--- src/main/java/com/example/filter/AuthFilter.java
+++ src/main/java/com/example/filter/AuthFilter.java
@@ -1,5 +1,37 @@
 package com.example.filter;
 
-public class AuthFilter {
-    //write your code here!
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class AuthFilter implements Filter {
+
+    private FilterConfig filterConfig;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        this.filterConfig = filterConfig;
+        System.out.println("AuthFilter инициализирован");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        HttpSession session = httpRequest.getSession();
+
+        if (session.getAttribute("user") == null) {
+            httpResponse.sendRedirect(httpRequest.getContextPath() + "/login");
+        } else {
+            chain.doFilter(request, response);
+        }
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("AuthFilter уничтожен");
+    }
 }
--- src/main/java/com/example/servlet/LoginServlet.java
+++ src/main/java/com/example/servlet/LoginServlet.java
@@ -1,5 +1,34 @@
 package com.example.servlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
-public class LoginServlet {
-    //write your code here!
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        if (session.getAttribute("user") != null) {
+            resp.sendRedirect("/login.jsp");
+        } else {
+            resp.sendRedirect("/user/hello.jsp");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
+
+        if (login.equals("admin") && !password.isEmpty()) {
+            HttpSession session = req.getSession();
+            session.setAttribute("user", login);
+            resp.sendRedirect("/user/hello.jsp");
+        } else {
+            req.getRequestDispatcher("/login.jsp").forward(req, resp);
+        }
+    }
 }
